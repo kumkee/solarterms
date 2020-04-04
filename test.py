8eb6af8fcb289d0c1c5c64c47e06c0ff932351e4
Jinjun Liang <kumkeejjl@gmail.com>
2020-04-05 10:15:45 +1200

    +arrow in test.py

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import solarterms
 import datetime
+import arrow
 
 year = 2020
 dt = datetime.timedelta(days=366)
-t = datetime.datetime(year, 1, 1, tzinfo=datetime.timezone.utc)
+t = arrow.Arrow(year, 1, 1, tzinfo='Pacific/Auckland')
 st = solarterms.SolarTerms(baseTime=t, backspan=datetime.timedelta(), forwardspand=dt)
 
 print('Solar terms for {year} are (in {tz}):'.format(year=year, tz=st.tzname()))
