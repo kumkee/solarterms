3e927d6519bc6b549d25193aa13199c5981e1288
Jinjun Liang <kumkeejjl@gmail.com>
2020-04-11 16:21:38 +1200

    command line argument to specify year and timezone

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import solarterms
 import datetime
 import arrow
+import sys
 
-year = 2021
+year = int(sys.argv[1]) if len(sys.argv) >=2 else 2021
 dt = datetime.timedelta(days=366)
-t = arrow.Arrow(year, 1, 1, tzinfo='Australia/Sydney')
+tz = sys.argv[2] if len(sys.argv) >=3 else 'Australia/Sydney'
+t = arrow.Arrow(year, 1, 1, tzinfo=tz)
 st = solarterms.SolarTerms(baseTime=t, backspan=datetime.timedelta(), forwardspand=dt)
 
 print('Solar terms for {year} will be (in the timezone of {tz}):'.format(year=year, tz=st.tzname()))
